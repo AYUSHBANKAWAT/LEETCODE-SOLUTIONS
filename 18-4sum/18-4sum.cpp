4d9d331f3870203856a417948c0941351cdd3f21
AYUSHBANKAWAT <82307560+AYUSHBANKAWAT@users.noreply.github.com>
2022-05-08 01:39:18 +0530

    Time: 62 ms (75.18%), Space: 13.2 MB (77.27%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -24,13 +24,13 @@ public:
                         temp[3]=nums[right];
                         ans.push_back(temp);
                         
-                        while(left<right && nums[left]==temp[2])++left;
-                        while(left<right && nums[right]==temp[3])--right; 
+                        while(left<right && nums[left]==temp[2])left++;
+                        while(left<right && nums[right]==temp[3])right--; 
                     }
                 }
-                while( j+1<n && nums[j]==nums[j+1] )++j;
+                while( j+1<n && nums[j]==nums[j+1] )j++;
             }
-            while( i+1<n && nums[i]==nums[i+1] )++i;
+            while( i+1<n && nums[i]==nums[i+1] )i++;
         }
         return ans;
     }
