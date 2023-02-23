58827a0f8cac6cf345e8d2712b0780d0854e7318
AYUSHBANKAWAT <82307560+AYUSHBANKAWAT@users.noreply.github.com>
2023-02-23 15:49:58 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -8,46 +8,18 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
+    int f( int i,int * a,vector<int>&dp ){
+        if( i==0 )return a[0];
+        if( i<0 )return 0;
+        if( dp[i]!=-1 )return dp[i];
+        int pick = a[i]+f( i-2,a,dp );
+        int not_pick = f( i-1,a,dp );
+        return dp[i] = max(pick,not_pick  );
+    }
 	// calculate the maximum sum with out adjacent
 	int findMaxSum(int *arr, int n) {
-	    int curr_max,pre_max,curr_i,pre_i;
-	    if( arr[n-1]>arr[n-2] ){
-	        curr_max =arr[n-1];
-	        curr_i=n-1;
-	        pre_max=arr[n-2];
-	        pre_i=n-2;
-	    }
-	    else{
-	        curr_max =arr[n-2];
-	        curr_i=n-2;
-	        pre_max=arr[n-1];
-	        pre_i=n-1;
-	    }
-	    for( int i = n-3;i>=0;i-- ){
-	        if( curr_i-i==1 ){
-	           int pos_max = pre_max+arr[i];
-	           //int pos_i = i;
-	           if( pos_max>=curr_max ){
-	               pre_max = curr_max;
-	               pre_i = curr_i;
-	               curr_max = pos_max;
-	               curr_i = i;
-	           }else{
-	               pre_max = pos_max;
-	               pre_i = i;
-	           }
-	        }else{
-	            int pos_max = curr_max+arr[i];
-	            pre_max = curr_max;
-	            pre_i = curr_i;
-	            curr_max = pos_max;
-	            curr_i = i;
-	        }
-	        //cout<<curr_max<<"  "<<pre_max<<endl;
-	    }
-	    return max( curr_max,pre_max );
-	    
-	    
+	    vector<int>dp(n,-1);
+	    return f(n-1,arr,dp);
 	}
 };
 
